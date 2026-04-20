171bdd7cb9031a74e54aab7e4531af25ddfb0ea4
RoBlazMarina <rodriguez.blazquez.marina@gmail.com>
2026-04-20 10:14:25 +0200

    Pruebas20/04-10:14

--- src/ClasesApp/VentanaPrincipal.java
+++ src/ClasesApp/VentanaPrincipal.java
@@ -6,11 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JFormattedTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	public static JTextField CuadroTexto;
 
 	/**
 	 * Launch the application.
@@ -37,7 +47,27 @@ public class VentanaPrincipal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Enviar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Ventana2 x=new Ventana2();
+				x.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(172, 100, 89, 23);
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Ponga aquí su nombre");
+		lblNewLabel.setBounds(160, 30, 111, 14);
+		contentPane.add(lblNewLabel);
+		
+		CuadroTexto = new JTextField();
+		CuadroTexto.setBounds(172, 55, 86, 20);
+		contentPane.add(CuadroTexto);
+		CuadroTexto.setColumns(10);
 
 	}
-
 }
